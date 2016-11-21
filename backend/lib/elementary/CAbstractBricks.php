2c918f607da5b8dcebb95b11ae78ff75b6d1b382
Rafik Rushanian <rafik.rushanian@gmail.com>
2016-11-21 21:21:40 +0400

    Branch we use for elementary parts of system * Added CAbstractBricks * Removed file interfaces.php

--- backend/lib/elementary/CAbstractBricks.php
+++ backend/lib/elementary/CAbstractBricks.php
@@ -2,34 +2,92 @@
 
     namespace lib\elementary;
 
+    /**
+     * Interface IBricks
+     *
+     * @package lib\elementary
+     */
+    interface IBricks{
+
+        /**
+         * @param array|null $args
+         *
+         * @return mixed
+         */
+        public function Initial(array $args=null);
+
+        /**
+         * @param $key
+         * @param null $in_area
+         * @return CResponse
+         */
+        public function getBlock($key, $in_area = null);
+
+        /**
+         * @param $key
+         * @param $value
+         * @param null $in_area
+         * @return CResponse
+         */
+        public function setBlock($key, $value, $in_area = null);
+    }
+
+    /**
+     * Class CAbstractBricks
+     * @package lib\elementary
+     */
     class CAbstractBricks implements IBricks {
 
         protected $storage = [];
 
+        /**
+         * @param array|null $args
+         * @return void
+         */
         public function Initial (array $args = null) {
-            // TODO: Implement Initial() method.
             if ($args) {
                 $this->storage = $args;
             }
         }
 
-        public function getBlock ($key) {
-            // TODO: Implement getBlock() method.
+        /**
+         * @param $key
+         * @param null $in_area
+         * @return CResponse
+         */
+        public function getBlock ($key, $in_area = null) {
             $response = new CResponse();
-            if (array_key_exists($key, $this->storage)) {
-
-            } else {
-                $response->setResponseStatus(false);
+            if($in_area){
+                (isset($this->storage[$in_area][$key]))?
+                    $response->setData($this->storage[$in_area][$key]): $response->setResponseStatus(false);
             }
-
+            else{
+                (isset($this->storage[$key]))?
+                    $response->setData($this->storage[$key]): $response->setResponseStatus(false);
+            }
             return $response;
         }
 
-        public function setBlock ($key, $value) {
-            // TODO: Implement setBlock() method.
+        /**
+         * @param $key
+         * @param $value
+         * @param null $in_area
+         * @return void
+         */
+        public function setBlock ($key, $value, $in_area = null) {
+            ($in_area)? $this->storage[$in_area][$key] = $value: $this->storage[$key][$value];
         }
 
-        private function walkForKey($key, array $block, $onAllBlock = false){
+        /**
+         * @return CResponse
+         */
+        public function getStorage(){
+            $return = new CResponse($this->storage);
+            return $return;
+        }
+
+        //TODO - Do recursive search for key
+/*        private function walkForKey($key, array $blocks, $onAllBlock = false){
             $response = new CResponse();
             if($onAllBlock){
                 foreach ($block as $cur_key=>$values){
@@ -44,5 +102,5 @@
                 else $response->setResponseStatus(false);
             }
             return $response;
-        }
+        }*/
     }
--- backend/lib/elementary/CGet.php
+++ backend/lib/elementary/CGet.php
@@ -2,27 +2,6 @@
 
 namespace lib\elementary;
 
-class CGet implements IBricks {
-    protected $storage = [];
+class CGet extends CAbstractBricks{
 
-    public function Initial (array $args = null) {
-        // TODO: Implement Initial() method.
-        if($args) $this->storage = $args;
-    }
-
-    public function getBlock ($key) {
-        // TODO: Implement getBlock() method.
-        $response = new CResponse();
-        if(array_key_exists($key, $this->storage)){
-
-        }
-        else{
-            $response->setResponseStatus(false);
-        }
-        return $response;
-    }
-
-    public function setBlock ($key, $value) {
-        // TODO: Implement setBlock() method.
-    }
 }
